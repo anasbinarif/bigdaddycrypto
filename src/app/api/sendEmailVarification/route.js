bae65b406e9b57985b9f6e63e1f0f07a9a4e73b7
anasbinarif <58784109+anasbinarif@users.noreply.github.com>
2024-06-26 01:04:16 +0200

    smtp done

--- src/app/api/sendEmailVarification/route.js
+++ src/app/api/sendEmailVarification/route.js
@@ -18,7 +18,9 @@ export async function POST(req, res) {
         const verifyUrl = `${process.env.NEXT_PUBLIC_URI}/verify?token=${token}&email=${email}`;
 
         const transporter = nodemailer.createTransport({
-            service: "gmail",
+            host: process.env.EMAIL_SERVER_HOST,
+            port: process.env.EMAIL_SERVER_PORT,
+            secure: true,
             auth: {
                 user: process.env.EMAIL_SERVER_USER,
                 pass: process.env.EMAIL_SERVER_PASSWORD,
