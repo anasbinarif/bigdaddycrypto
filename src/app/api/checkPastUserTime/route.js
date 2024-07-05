831542e7abb3dd75afc96b8cc30aa57304833dce
Turing-26 <mail2sarim@gmail.com>
2024-07-05 10:15:42 +0500

    .

--- src/app/api/checkPastUserTime/route.js
+++ src/app/api/checkPastUserTime/route.js
@@ -10,12 +10,12 @@ export async function POST(req) {
   try {
     // Get the current time in Berlin
     const berlinNow = DateTime.now().setZone(berlinTimeZone);
-    console.log(
-      "berlinNow",
-      berlinNow.toString(),
-      "targetDate",
-      targetDate.toString()
-    );
+    // console.log(
+    //   "berlinNow",
+    //   berlinNow.toString(),
+    //   "targetDate",
+    //   targetDate.toString()
+    // );
 
     if (berlinNow < targetDate) {
       return NextResponse.json(
